4537cfb3ae385dc54be47e169f6de0ef9785bd0f
Daisie <dacekebzere@gmail.com>
2023-04-30 17:22:33 +0200

    changed the messages, added guess rank depending on the top number

--- number_guess.py
+++ number_guess.py
@@ -1,10 +1,10 @@
 import random
 
-print("Welcome to my Harry Potter Number Guessing Game!")
-print("Get ready to test your powers! 🔮")
+print("\nWelcome to my Harry Potter Number Guessing Game!")
+print("Get ready to test your magical powers! 🧙✨🔮\n")
 
 def play_game():
-    top_of_range = input("Type a number: ")
+    top_of_range = input("Enter the highest number for your magical range: ")
 
     if top_of_range.isdigit():
         top_of_range = int(top_of_range)
@@ -21,7 +21,7 @@ def play_game():
 
     while True:
         guesses += 1
-        user_guess = input("Guess a number: ")
+        user_guess = input("Cast your guess: ")
         if user_guess.isdigit():
             user_guess = int(user_guess)
         else:
@@ -29,22 +29,22 @@ def play_game():
             continue
         
         if user_guess == random_number:
-            print("You got it!")
+            print("\nYou got it! 🎉 Guessus correctus!\n")
             break
         elif user_guess > random_number:
-            print("You were above the number!")
+            print("Your guess soared higher than The Golden Snitch! ʚ🟡ɞ Try a lower guess!")
         else:
-            print("You were below the number!")
-
-    print("You got it in", guesses, "guesses.")
-    if guesses > 2:
-        print("Not very psycic of you...")
+            print("Your guess sank lower than merpoeple in The Black Lake! 🧜‍ Try a higher guess!")
+    
+    if guesses > round(top_of_range * 0.2):
+        print("But... Not very psychic of you. You cast the right spell in", guesses, "guesses... Witchery might not be your strength.")
     else:
-        print("Wow, you might have the powers!")
+        print("Wow, you might have the true magical powers! 🧙✨ You cast the right spell in", guesses, "guesses...")
+
 
 while True:
     play_game()
-    play_again = input("Do you want to play again? (yes/no): ")
+    play_again = input("Do you dare to play again? (yes/no): ")
     if play_again.lower() != "yes":
-        print("Thanks for playing! :)")
+        print("Thanks for testing your magic!")
         break
